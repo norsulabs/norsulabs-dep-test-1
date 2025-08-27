04cf82d5cb2c92ba40b723ffde5fc7613336875f
ameen-91 <mohammedameen9011@gmail.com>
2025-08-27 16:33:57 +0530

    add docker file

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.12-slim
+
+COPY . /app
+
+WORKDIR /app
+
+RUN pip install uv
+
+RUN uv sync
+
+EXPOSE 8501
+
+CMD ["uv", "run", "streamlit", "run", "main.py", "--server.headless", "true", "--server.port", "8501"]
--- main.py
+++ main.py
@@ -10,11 +10,3 @@ def main():
 
     env_var = os.getenv('MY_ENV_VAR', 'Default Value')
     st.write(f"Environment Variable MY_ENV_VAR: {env_var}")
-
-if __name__ == "__main__":
-    if 'STREAMLIT_RUN' not in os.environ:
-        os.environ['STREAMLIT_RUN'] = '1'
-        subprocess.Popen([sys.executable, "-m", "streamlit", "run", __file__, "--server.headless", "true", "--server.port", "8501"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        print("Streamlit app started at http://localhost:8501")
-    else:
-        main()
